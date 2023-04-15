063535e19c2301318cb1fde1ba9eefe140737b35
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-15 02:47:49 -0400

    write php server code

---
+++ foodCheck.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Content-Type: text/xml');
+
+echo '<response>';
+
+$item = $_GET['item'];
+
+$itemArray = array('burger', 'pizza', 'milk', 'bread', 'coke');
+
+if (in_array($item, $itemArray))
+  {
+  echo "We do have a $item in stock";
+  }
+
+elseif($item="") {
+    echo 'Enter something';
+  }
+
+else
+  {
+  echo "We do not have a $item in stock";
+  }
+
+echo '</response>';
+
+?>

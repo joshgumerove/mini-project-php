502a2e23bec4d4e13761444731175879016ec97d
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-14 22:53:30 -0400

    setup ajax request

--- food.js
+++ food.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+let xhr = new XMLHttpRequest();
+
+if (!xhr) {
+  alert("you are not set up with a proper browser");
+}
+
+function execute() {
+  if (xhr.readyState == 0 || xhr.readyState == 4) {
+    // unsent or done
+    let foodItem = document.getElementById("foodItem").value;
+    let method = "GET";
+    let url = "will come back";
+
+    xhr.open(method, url);
+    xhr.onreadystatechange = () => {};
+    xhr.send();
+  } else {
+    setTimeout(() => {
+      console.log("no AJAX request sent");
+      execute();
+    }, 1000);
+  }
+}
